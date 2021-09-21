fe07ce6152d65ecc4ee022e645a83327a938c1c8
REKO7 <71840491+REKO7@users.noreply.github.com>
2021-09-21 06:11:04 +0300

    Update help.js

--- commands/🌀 Info/help.js
+++ commands/🌀 Info/help.js
@@ -33,7 +33,7 @@ const embed = new MessageEmbed()
 .setTitle("**__Help__ **") 
     .setFooter(message.author.username)
   .setImage("https://cdn.discordapp.com/attachments/808760849867079721/838228786742820904/standard_1.gif") 
-  .setDescription(`» Prefix: *help\n\n[  **Support**  ](https://discord.gg/b3dmQYX5PH) - [   **Invite**   ](https://discord.com/api/oauth2/authorize?client_id=769330211157835784&permissions=8&scope=bot) - [  **Vote**  ](https://top.gg/bot/769330211157835784/vote)`)
+  .setDescription(` Prefix: *help\n\n**[  Support  ](https://discord.gg/b3dmQYX5PH)** - **[   Invite   ](https://discord.com/api/oauth2/authorize?client_id=769330211157835784&permissions=8&scope=bot)** - **[  Vote  ](https://top.gg/bot/769330211157835784/vote)**`)
 
 .addField("<a:SIA_DiscordStaff:803965271554392114> | Setup ","`setup` - `reset`")
 

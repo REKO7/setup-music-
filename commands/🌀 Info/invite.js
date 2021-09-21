ef880f65ffc57ae65a9b2f75a04d6a8fb3627eb4
REKO7 <71840491+REKO7@users.noreply.github.com>
2021-09-21 04:07:53 +0300

    Update invite.js

--- commands/🌀 Info/invite.js
+++ commands/🌀 Info/invite.js
@@ -19,7 +19,7 @@ module.exports = {
             .setAuthor(message.author.username)
             .setDescription(`\`Click invite Music Bot✨ \``)
             .addField("**Onwer Bot ✨**", `
-                >>> <@747528109884178532> \`!   𝗙𝗞・𝗠𝗿.𝗥𝗲𝗞𝗼#1111 \` [\`Music Bot Support\`](https://discord.com/api/oauth2/authorize?client_id=769330211157835784&permissions=8&scope=bot)
+                >>> <@747528109884178532> \`\` [\`Music Bot Support\`](https://discord.com/api/oauth2/authorize?client_id=769330211157835784&permissions=8&scope=bot)
                 `)
             .setFooter("Music Bot✨")
 
